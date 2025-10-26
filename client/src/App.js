2a2700dbe9ffa1d43875929e954d0185e3e01662
Bhanuka Lakshitha Dayananda <99331889+IT21307058@users.noreply.github.com>
2025-10-26 08:38:24 +0530

    remove unnessary file changes commited

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import { ToastContextProvider } from "./context/ToastContext";
-// import AddPost from "./pages/AddPost";
-import ProtectedRoute from "./components/ProtectedRoute";
+
 
 
 function App() {
@@ -17,9 +14,6 @@ function App() {
         <ToastContextProvider>
           <Layout>
             <Switch>
-
-              {/* <Route path="/login" element={<Login />} /> */}
-              {/* <Route path="/register" element={<Register />} /> */}
               <Route path="/" element={
                   <Home />
               } />
